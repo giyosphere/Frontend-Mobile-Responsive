1af309378b53c26d806e92954e80e856a65cd606
Gio Dalaoyan <giodalaoyan@gmail.com>
2026-01-21 21:52:51 +0800

    Adding javascript code for the responsive navigation bar

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+  const navlinks = document.querySelector('.navigation .nav-links');
+  navlinks.style.display = 'flex';
+}
+
+
+function closeSidebar(){
+  const navlinks = document.querySelector('.navigation .nav-links');
+  navlinks.style.display = 'none';
+}
