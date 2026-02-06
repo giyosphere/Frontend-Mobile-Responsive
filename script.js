846819076b5bc35754b73a0a34f9037be7f08af1
Gio Dalaoyan <giodalaoyan@gmail.com>
2026-02-06 11:48:17 +0800

    Update responsiveness

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
-
-
-
 function showSidebar(){
   const navlinks = document.querySelector('.navigation .nav-links');
   navlinks.style.display = 'flex';
 }
-
-
 function closeSidebar(){
   const navlinks = document.querySelector('.navigation .nav-links');
   navlinks.style.display = 'none';
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
